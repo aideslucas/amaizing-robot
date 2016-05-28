1b979fe6fe4ba7b172f23820bcbdbef1290e9efe
eylamm <eylam7100@gmail.com>
2016-05-28 05:50:20 -0700

    Robot header with errors

--- LoadMap/Debug/subdir.mk
+++ LoadMap/Debug/subdir.mk
@@ -9,6 +9,7 @@ CPP_SRCS += \
 ../Main.cpp \
 ../Map.cpp \
 ../Node.cpp \
+../Robot.cpp \
 ../lodepng.cpp 
 
 OBJS += \
@@ -17,6 +18,7 @@ OBJS += \
 ./Main.o \
 ./Map.o \
 ./Node.o \
+./Robot.o \
 ./lodepng.o 
 
 CPP_DEPS += \
@@ -25,6 +27,7 @@ CPP_DEPS += \
 ./Main.d \
 ./Map.d \
 ./Node.d \
+./Robot.d \
 ./lodepng.d 
 
 
---
+++ LoadMap/Robot.cpp
@@ -0,0 +1,18 @@
+/*
+ * Robot.cpp
+ *
+ *  Created on: May 28, 2016
+ *      Author: colman
+ */
+
+#include "Robot.h"
+
+Robot::Robot() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Robot::~Robot() {
+	// TODO Auto-generated destructor stub
+}
+
