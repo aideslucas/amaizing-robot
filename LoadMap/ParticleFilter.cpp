25d6455581f861dfac19a27a708f494a16395215
eagleluck <aideslucas@gmail.com>
2016-06-03 13:37:16 -0700

    Particle Filter - only struct for particle

---
+++ LoadMap/ParticleFilter.cpp
@@ -0,0 +1,18 @@
+/*
+ * ParticleFilter.cpp
+ *
+ *  Created on: Jun 3, 2016
+ *      Author: colman
+ */
+
+#include "ParticleFilter.h"
+
+ParticleFilter::ParticleFilter() {
+	// TODO Auto-generated constructor stub
+
+}
+
+ParticleFilter::~ParticleFilter() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ LoadMap/ParticleFilter.h
@@ -0,0 +1,26 @@
+/*
+ * ParticleFilter.h
+ *
+ *  Created on: Jun 3, 2016
+ *      Author: colman
+ */
+
+#ifndef PARTICLEFILTER_H_
+#define PARTICLEFILTER_H_
+#include "Map.h"
+
+typedef struct particle
+{
+	cordinates cordinate;
+	double weight;
+	char state;
+}
+Particle;
+
+class ParticleFilter {
+public:
+	ParticleFilter();
+	virtual ~ParticleFilter();
+};
+
+#endif /* PARTICLEFILTER_H_ */
